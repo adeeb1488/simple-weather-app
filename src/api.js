ed5d13222a48b23ecc79bbddddec8c1984b98119
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-19 21:37:55 -0400

    Creating city search functionality component

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'baf98edfccmshee405e6f7d29fc8p167db1jsna20dd109f711',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
